6bb3b0f6db59ed0b0ad1830af3e0d9862e063ac4
AlekSimpson <alek@asu.me>
2023-10-15 18:58:58 -0700

    stashing

--- chess.hs
+++ chess.hs
@@ -17,41 +17,51 @@ instance Show Piece where
 type Square = (Int, Int, SquareState) 
 type Board = [[Square]]
 
-
 makeRow :: Int -> [(Int, Int, SquareState)]
-makeRow x = [(0, x, Empty), (1, x, Empty), (2, x, Empty), (3, x, Empty), (4, x, Empty), (5, x, Empty), (6, x, Empty), (7, x, Empty)]
+makeRow x = [(1, x, Empty), (2, x, Empty), (3, x, Empty), (4, x, Empty), (5, x, Empty), (6, x, Empty), (7, x, Empty), (8, x, Empty)]
+
 
 -- create chess board function
-createChessBoard i row board | i == 0 = board -- base case
-                             | not (i == 0) = createChessBoard (i-1) (makeRow (i-1)) (row : board)
-initBoard :: Board
-initBoard = createChessBoard 9 (makeRow 9) [] -- helper function
+-- makeBoard :: Int -> Board -> Board
+-- makeBoard 0 board = board
+-- makeBoard count board = makeBoard (count-1) ((makeRow count) ++ board)
 
---querySquare square =  (square )
---query board row = map querySquare (board !! row)
-
+-- Print Row function
 printRow :: Int -> [Square] -> IO ()
-printRow 0 row = 
-  do 
-    putStr (show (row !! 0))
-    print ("\n")
+printRow 0 row = do putStrLn (show (row !! 0))
 printRow i row = 
   do 
     putStr (show (row !! i))
     printRow (i-1) row
 
-printMatrix :: Board -> Int -> IO () 
-printMatrix matrix 0 = do putStrLn ""
-printMatrix matrix i = 
+-- Print Matrix function
+printMatrixHelper :: Board -> Int -> IO () 
+printMatrixHelper matrix 0 = do putStrLn ""
+printMatrixHelper matrix i = 
   do 
     printRow i (matrix !! i)
-    printMatrix matrix (i-1)
+    printMatrixHelper matrix (i-1)
 
 printMatrix :: Board -> IO ()
-printMatrix matrix = printMatrix matrix 9
+printMatrix matrix = printMatrixHelper matrix 7
 
-
+-- main
 main = do
-  let board = initBoard
-  printMatrix board
-  print "success"
+  -- let board = makeBoard 9 []
+  -- printMatrix board
+  let board = [makeRow 9]
+  let row = makeRow 9
+  print (show (row ++ board))
+
+
+
+
+
+
+
+
+
+
+
+
+
